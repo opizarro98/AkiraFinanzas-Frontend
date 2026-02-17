3548cfa9389a54a6320eefb6ad8cd3b20c71ff54
Oscar Pizarro <oscar.pizarro@ucuenca.edu.ec>
2026-02-17 01:12:48 -0500

    Refactor Movements component to use MovementResponseDTO and update data binding in the template

--- src/app/shared/components/movements/movements.form.component.ts
+++ src/app/shared/components/movements/movements.form.component.ts
@@ -3,16 +3,10 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../ui/button/button.component';
 import { TableDropdownComponent } from '../common/table-dropdown/table-dropdown.component';
 import { BadgeComponent } from '../ui/badge/badge.component';
-interface Transaction {
-    image: string;
-    action: string;
-    date: string;
-    amount: string;
-    category: string;
-    status: "Success" | "Pending" | "Failed";
-}
-
-
+import { MovementResponseDTO } from '../../models/movements/MovementResponseDTO';
+import { MovementService } from '../../services/movements/movement.service';
+import { NotificationService } from '../../services/notification.service';
+import { MovementTypeEnum } from '../../models/MovementTypeEnum';
 @Component({
     selector: 'app-movements-form',
     imports: [
@@ -25,142 +19,41 @@ interface Transaction {
     styles: ``
 })
 export class MovementsFormComponent {
-    // Type definition for the transaction data
-
-
-    transactionData: Transaction[] = [
-        {
-            image: "/images/brand/brand-08.svg", // Path or URL for the image
-            action: "Bought PYPL", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Success",
-        },
-        {
-            image: "/images/brand/brand-07.svg", // Path or URL for the image
-            action: "Bought AAPL", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Pending",
-        },
-        {
-            image: "/images/brand/brand-15.svg", // Path or URL for the image
-            action: "Sell KKST", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Success",
-        },
-        {
-            image: "/images/brand/brand-02.svg", // Path or URL for the image
-            action: "Bought FB", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Success",
-        },
-        {
-            image: "/images/brand/brand-10.svg", // Path or URL for the image
-            action: "Sell AMZN", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Failed",
-        },
-        {
-            image: "/images/brand/brand-08.svg", // Path or URL for the image
-            action: "Bought PYPL", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Success",
-        },
-        {
-            image: "/images/brand/brand-07.svg", // Path or URL for the image
-            action: "Bought AAPL", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Pending",
-        },
-        {
-            image: "/images/brand/brand-15.svg", // Path or URL for the image
-            action: "Sell KKST", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Success",
-        },
-        {
-            image: "/images/brand/brand-02.svg", // Path or URL for the image
-            action: "Bought FB", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Success",
-        },
-        {
-            image: "/images/brand/brand-10.svg", // Path or URL for the image
-            action: "Sell AMZN", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Failed",
-        },
-        {
-            image: "/images/brand/brand-08.svg", // Path or URL for the image
-            action: "Bought PYPL", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Success",
-        },
-        {
-            image: "/images/brand/brand-07.svg", // Path or URL for the image
-            action: "Bought AAPL", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Pending",
-        },
-        {
-            image: "/images/brand/brand-15.svg", // Path or URL for the image
-            action: "Sell KKST", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Success",
-        },
-        {
-            image: "/images/brand/brand-02.svg", // Path or URL for the image
-            action: "Bought FB", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Success",
-        },
-        {
-            image: "/images/brand/brand-10.svg", // Path or URL for the image
-            action: "Sell AMZN", // Action description
-            date: "Nov 23, 01:00 PM", // Date and time of the transaction
-            amount: "$2,567.88", // Transaction amount
-            category: "Finance", // Category of the transaction
-            status: "Failed",
-        },
-    ]
-
+    movementData: MovementResponseDTO[] = []; // Aquí se almacenarán los movimientos obtenidos del servicio
     currentPage = 1;
-    itemsPerPage = 5;
+    itemsPerPage = 10;
 
-    get totalPages(): number {
-        return Math.ceil(this.transactionData.length / this.itemsPerPage);
+
+    constructor(private movementService: MovementService, private notificationService: NotificationService) {
+
     }
 
-    get currentItems(): Transaction[] {
+
+    ngOnInit() {
+        this.loadMovements();
+    }
+
+
+
+    loadMovements() {
+        this.movementService.getMovements().subscribe(
+            (data) => {
+                this.movementData = data;
+                console.log('Movements loaded:', this.movementData);
+            },
+            () => {
+                this.notificationService.show('error', 'Error', 'No se pudieron cargar los movimientos, intenta nuevamente más tarde.');
+            }
+        );
+    }
+
+    get totalPages(): number {
+        return Math.ceil(this.movementData.length / this.itemsPerPage);
+    }
+
+    get currentItems(): MovementResponseDTO[] {
         const start = (this.currentPage - 1) * this.itemsPerPage;
-        return this.transactionData.slice(start, start + this.itemsPerPage);
+        return this.movementData.slice(start, start + this.itemsPerPage);
     }
 
     goToPage(page: number) {
@@ -169,19 +62,30 @@ export class MovementsFormComponent {
         }
     }
 
-    handleViewMore(item: Transaction) {
+    handleViewMore(item: MovementResponseDTO) {
         // logic here
         console.log('View More:', item);
     }
 
-    handleDelete(item: Transaction) {
+    handleDelete(item: MovementResponseDTO) {
         // logic here
         console.log('Delete:', item);
     }
 
     getBadgeColor(status: string): 'success' | 'warning' | 'error' {
-        if (status === 'Success') return 'success';
-        if (status === 'Pending') return 'warning';
+        if (status === 'INCOME') return 'success';
+        if (status === 'TRANSFER') return 'warning';
         return 'error';
     }
+
+    // Metodo para obtener la etiqueta legible del tipo de movimiento
+    getMovementTypeLabel(type: MovementTypeEnum): string {
+        return this.movementTypeLabelMap[type] ?? type;
+    }
+    // Mapeo para convertir los valores del enum en etiquetas legibles
+    movementTypeLabelMap: Record<MovementTypeEnum, string> = {
+        [MovementTypeEnum.Egreso]: 'GASTO',
+        [MovementTypeEnum.Ingreso]: 'INGRESO',
+        [MovementTypeEnum.Transferencia]: 'TRANSFERENCIA'
+    };
 }
--- src/app/shared/models/movements/MovementResponseDTO.ts
+++ src/app/shared/models/movements/MovementResponseDTO.ts
@@ -3,7 +3,6 @@ import { MovementTypeEnum } from "../MovementTypeEnum";
 export interface MovementResponseDTO {
     movementId: string;
     type: MovementTypeEnum;
-    date: string;
     amount: string;
     description: string;
     movementDate: string;
